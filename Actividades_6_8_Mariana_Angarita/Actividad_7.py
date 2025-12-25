bfa2ef41bae427428b03d2bb1475cd6715cb9a8a
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2025-12-25 17:26:59 +0100

    Add info method to Libro class and implement menu functionality for managing books

--- Actividades_6_8_Mariana_Angarita/Actividad_7.py
+++ Actividades_6_8_Mariana_Angarita/Actividad_7.py
@@ -38,6 +38,9 @@ class Libro():
     def get_paginas(self):
         return self.paginas
     
+    def info(self):
+        return (f"Titulo: {self.get_titulo()}, Autor: {self.get_autor()}, Género: {self.get_genero()}, Número de Páginas: {self.get_genero()}")
+    
     def set_titulo(self,titulo):
         self.titulo=titulo
     
@@ -50,3 +53,58 @@ class Libro():
     def set_paginas(self, paginas):
         self.paginas=paginas
     
+menu=["ATRIBUTOS LIBRO", "INFO", "SALIR"]
+
+lista_libros=[]
+
+def catalogo_libros():
+    for indice, valor in enumerate(lista_libros):
+        return(f"Pulsa {indice}: {valor}")
+
+def menu_principal():
+
+    print("***************************")
+
+    print("MENÚ PRINCIPAL")
+
+    for indice,valor in enumerate(menu,1):
+        return(f"Pulsa {indice}: {valor}")
+    
+    print("***************************")
+
+salir=False
+
+while not salir:
+    menu_principal()
+
+    opcion=int(input("Elige una opción: "))
+
+    match opcion:
+
+        case 1:
+
+            print(f"Has elegido {menu[opcion-1]}")
+
+            titulo=input("Indica el título del libro: ").lower()
+            autor=input("Indica el autor del libro").lower()
+            genero=input("Indica el género del libro: ").lower()
+            paginas=int(input("Indica el número de páginas del libro: "))
+
+            libro=Libro(titulo,autor,genero,paginas)
+
+            lista_libros.append(libro)
+
+        case 2:
+            print(f"Has elegido {menu[opcion-1]}")
+
+            if len(lista_libros)>0:
+                libro_elegido=int(input("Elige una opción: "))
+            else:
+                print("Lista vacía, ve primero a la opción 1")
+
+        case 3:
+            print(f"Has elegido {menu[opcion-1]}")
+        case other:
+            print("Opción no válida, pulsa 1, 2 o 3")
+
+
