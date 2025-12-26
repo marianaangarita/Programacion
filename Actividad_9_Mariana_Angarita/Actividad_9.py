1eb50d6542e72c6aed94dbed84940ab889376e84
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2025-12-26 12:00:09 +0100

    Refactor Tablet class constructor and methods

--- Actividad_9_Mariana_Angarita/Actividad_9.py
+++ Actividad_9_Mariana_Angarita/Actividad_9.py
@@ -19,12 +19,30 @@ Elimina la app de la lista.
 '''
 
 class Tablet():
-    def __init__(self,creador,pantalla,cores,apps,status):
+    def __init__(self,creador,pantalla,cores):
         self.creador=creador
         self.tamano_pantalla=pantalla
         self.num_cores=cores
         self.apps=[]
-        self.status=True
+        self.status=False
+    
+    def get_creador(self):
+        return self.creador
+    
+    def get_tamano_pantalla(self):
+        return self.tamano_pantalla
+    
+    def get_num_cores(self):
+        return self.num_cores
+    
+    def get_apps(self):
+        return self.apps
+    
+    def get_status(self):
+        return self.status
+    
+    def info(self):
+        return(f"Creador: {self.get_creador()}, Tamaño pantalla: {self.get_tamano_pantalla()}, Número de cores: {self.get_num_cores()}, Apps: {self.get_apps()}, Status: {self.get_status()}")
     
     def power_on(self):
         if self.status==False:
@@ -40,7 +58,7 @@ class Tablet():
 
         else:
             self.apps.append(app)
-            print("La app se ha instalado correctamente.")
+            print(f"La app {app} se ha instalado correctamente.")
         
     def uninstall_app(self,app):
         
@@ -48,6 +66,24 @@ class Tablet():
 
             self.apps.remove(app)
 
-            print("La app se ha desinstalado correctamente")
+            print(f"La app {app} se ha desinstalado correctamente")
         else:
             print("La app no se encuentra en la lista, no se puede deinstalar")
+
+
+
+tablet1=Tablet("Steve Jobs", 13.5, 4)
+
+print(tablet1.info())
+
+tablet1.install_app("Procreate")
+
+tablet1.install_app("Final Cut")
+
+tablet1.install_app("Blender")
+
+tablet1.uninstall_app("Final Cut")
+
+tablet1.power_on()
+
+print(tablet1.info())
