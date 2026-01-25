904472a6077c1f9b77b2773518c65f8f6f2f6898
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-01-25 20:59:18 +0100

    ejercicio de examen

--- Simulacro_examen_gemini.py
+++ Simulacro_examen_gemini.py
@@ -71,4 +71,202 @@ Devolver un vehículo (buscando por matrícula).
 
 Salir.
 '''
+class Vehiculo():
+    def __init__(self, marca, modelo, matricula):
+        self.marca=marca
+        self.modelo=modelo
+        self.matricula=matricula
+        self.alquilado=False
+    def get_marca(self):
+        return self.marca
+    
+    def get_modelo(self):
+        return self.modelo
+    
+    def get_matricula(self):
+        return self.matricula
+    
+    def get_alquilado(self):
+        return self.alquilado
+    
+    def set_marca(self, marca):
+        self.marca=marca
 
+    def set_modelo(self, modelo):
+        self.modelo=modelo
+
+    def set_matricula(self, matricula):
+        self.matricula=matricula
+
+    def set_alquilado(self, alquilado):
+        self.alquilado=alquilado
+
+    def alquilar(self):
+        if self.get_alquilado()==False:
+            self.set_alquilado(True)
+            return (f"Se ha alquilado el vehículo con matrícula: {self.get_matricula()}")
+        else:
+            return(f"El vehículo con matricula: {self.get_matricula()}, ya estaba alquilado.")
+
+    def devolver(self):
+        if self.get_alquilado()==True:
+            self.set_alquilado(False)
+            return(f"Se ha devuelto el vehículo con matrícula: {self.get_matricula()}")
+        else:
+            return(f"El vehículo con matrícula: {self.get_matricula()}, no estaba alquilado.")
+    
+    def mostrar_info(self):
+        estado=""
+        if self.get_alquilado()==True:
+            estado="Alquilado"
+        else:
+            estado="Disponible"   
+        return(f"Vehículo: {self.get_marca()} {self.get_modelo()} | Matrícula: {self.get_matricula()} | Estado: {estado}")
+
+class Coche(Vehiculo):
+    def __init__(self, marca, modelo, matricula, puertas):
+        super().__init__(marca, modelo, matricula)
+        self.num_puertas=puertas
+    
+    def get_num_puertas(self):
+        return self.num_puertas
+    
+    def set_num_puertas(self, p):
+        self.num_puertas=p
+
+    def mostrar_info(self):
+        return (f"{super().mostrar_info()} | Número de puertas: {self.get_num_puertas()}")
+
+class Moto(Vehiculo):
+    def __init__(self, marca, modelo, matricula, cilindrada):
+        super().__init__(marca, modelo, matricula)
+        self.cilindrada=cilindrada
+    
+    def get_cilindrada(self):
+        return self.cilindrada
+    
+    def set_cilindrada(self, p):
+        self.cilindrada=p
+    
+    def mostrar_info(self):
+        return (f"{super().mostrar_info()} | Cilindrada: {self.get_cilindrada()} cc")
+
+class Flota():
+    def __init__(self):
+        self.lista_vehiculos=[]
+    
+    def agregar_vehiculo(self, vehiculo):
+        existe=False
+        for i in self.lista_vehiculos:
+            if i.get_matricula()==vehiculo.get_matricula():
+                existe=True
+                return(f"El vehiculo con matrícula: {vehiculo.get_matricula()} ya estaba en la lista.")
+        if existe==False:
+            self.lista_vehiculos.append(vehiculo)
+            return (f"Se ha agregado a la lista el vehículo con matrícula: {vehiculo.get_matricula()}.")
+    
+    def mostrar_flota(self):
+        for i in self.lista_vehiculos:
+            print(i.mostrar_info())
+        
+    
+    def buscar_por_matricula(self, matricula):
+        existe=False
+        for i in self.lista_vehiculos:
+            if i.get_matricula()==matricula:
+                existe=True
+                return(i.mostrar_info())
+        if existe==False:
+            return(f"El vehículo con matrícula: {matricula}, no está en la lista.")
+
+            
+        
+salir=False
+vehiculo=None
+flota=Flota()
+lista_menu=["CREAR Y REGISTAR COCHE", "CREAR Y REGISTRAR MOTO", "MOSTRAR FLOTA", "ALQUILAR VEHÍCULO", "DEVOLVER VEHÍCULO", "BUSCAR MATRÍCULA", "SALIR"]
+
+def menu():
+    print("***************************")
+    print("MENÚ PRINCIPAL")
+    for clave, valor in enumerate(lista_menu, 1):
+        print(f"Pulsa {clave}: {valor}")
+    print("***************************")
+
+while not salir:
+    menu()
+
+    opcion=int(input("Escoge una opción: "))
+
+    match opcion:
+        case 1:
+            print(f"Has elegido: {lista_menu[opcion-1]}")
+
+            marca=input("Indica la marca del coche: ").lower()
+            modelo=input("Indica el modelo: ").lower()
+            matricula=input("Indica la matrícula: ").lower()
+
+            vehiculo=Vehiculo(marca, modelo, matricula)
+            print(flota.agregar_vehiculo(vehiculo))
+
+        case 2:
+            print(f"Has elegido: {lista_menu[opcion-1]}")
+
+            marca=input("Indica la marca de la moto: ").lower()
+            modelo=input("Indica el modelo: ").lower()
+            matricula=input("Indica la matrícula: ").lower()
+
+            vehiculo=Vehiculo(marca, modelo, matricula)
+            print(flota.agregar_vehiculo(vehiculo))
+
+        case 3:
+            print(f"Has elegido: {lista_menu[opcion-1]}")
+            if flota.lista_vehiculos:
+                flota.mostrar_flota()
+            else:
+                print("Lista vacía, debes agregar primero un vehículo  a la lista.")
+
+        case 4:
+            print(f"Has elegido: {lista_menu[opcion-1]}")
+            matricula=input("Indica la matrícula: ").lower()
+            existe=False
+            if flota.lista_vehiculos:
+                for i in flota.lista_vehiculos:
+                    if i.get_matricula==matricula:
+                        existe=True
+                        print(i.alquilar())
+                        break
+                if existe==False:
+                    print(f"El coche con matricula: {matricula}, no está en la lista.")
+            else:
+                print("Lista vacía, agrega un vehículo para poder usar esta opción.")
+
+        case 5:
+            print(f"Has elegido: {lista_menu[opcion-1]}")
+            matricula=input("Indica la matrícula: ").lower()
+            existe=False
+            if flota.lista_vehiculos:
+                for i in flota.lista_vehiculos:
+                    if i.get_matricula==matricula:
+                        existe=True
+                        print(i.devolver())
+                        break
+                if existe==False:
+                    print(f"El coche con matricula: {matricula}, no está en la lista.")
+            else:
+                print("Lista vacía, agrega un vehículo para poder usar esta opción.")
+
+        case 6:
+            print(f"Has elegido: {lista_menu[opcion-1]}")
+
+            matricula=input("Indica la matrícula: ").lower()
+            if flota.lista_vehiculos:
+                print(flota.buscar_por_matricula(matricula))
+            else:
+                print("Lista vacía, debes agregar primero un vehículo a la lista.")
+        case 7:
+            print(f"Has elegido: {lista_menu[opcion-1]}")
+            salir=True
+            print("Has salido del programa.")
+        case other:
+            print("Opción incorrecta. Pulsa del 1 al 6.")
