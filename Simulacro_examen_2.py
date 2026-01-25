b3678224de6e571460a03a30cea2d9180941a73c
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-01-25 12:02:26 +0100

    añadir menu interactivo

--- Simulacro_examen_2.py
+++ Simulacro_examen_2.py
@@ -56,7 +56,7 @@ class Libro(Material):
     def set_num_paginas(self,num):
         self.num_paginas=num
     def mostrar_info(self):
-        return(f"Libro: {self.get_titulo()}, autor:{self.get_autor()}, año de publicación: {self.get_anio_publicacion()}, prestado: {self.get_prestado()}, número de páginas: {self.get_num_paginas()}")
+        return(f"{super().mostrar_info()}, número de páginas: {self.get_num_paginas()}")
     
 class Revista(Material):
     def __init__(self, titulo, autor, anio, num_edicion):
@@ -70,7 +70,7 @@ class Revista(Material):
         self.num_edicion=edicion
 
     def mostrar_info(self):
-        return(f"Libro: {self.get_titulo()}, autor:{self.get_autor()}, año de publicación: {self.get_anio_publicacion()}, prestado: {self.get_prestado()}, número de edición: {self.get_num_edicion()}")
+        return(f"{super().mostrar_info()}, número de edición: {self.get_num_edicion()}")
     
 class Biblioteca():
     def __init__(self):
@@ -107,3 +107,110 @@ class Biblioteca():
         else:
             return("Lista vacía, llena la lista antes de mostrar materiales.")
     
+biblioteca=Biblioteca()
+
+salir=False
+
+libro=None
+revista=None
+
+lista_menu=["CREAR LIBRO", "CREAR UNA REVISTA", "AGREGAR MATERIAL", "MOSTRAR MATERIAL", "BUSCAR POR TÍTULO", "PRESTAR MATERIAL", "DEVOLVER MATERIAL", "CAMBIAR TÍTULO", "CAMBIAR AUTOR", "CAMBIAR AÑO DE PUBLICACIÓN", "SALIR"]
+
+def menu():
+    print("******************************")
+    print("MENÚ PRINCIPAL")
+    for clave, valor in enumerate(lista_menu, 1):
+        print(f"Pulsa {clave}: {valor}")
+    print("******************************")
+
+while not salir:
+    menu()
+    opcion=int(input("Escoge una opción: "))
+    match opcion:
+        case 1:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            titulo=input("Indica el título del libro: ").lower()
+            autor=input("Indica el autor del libro: ").lower()
+            anio=int(input("Indica el año de publicación: "))
+            paginas=int(input("Indica el número de páginas: "))
+            if anio>=0 and paginas>0:
+                libro=Libro(titulo, autor, anio, paginas)
+            else:
+                print("Los datos introducidos no son correctos, el año y las páginas no pueden ser negativos.")
+        case 2:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            titulo=input("Indica el título del libro: ").lower()
+            autor=input("Indica el autor del libro: ").lower()
+            anio=int(input("Indica el año de publicación: "))
+            edicion=int(input("Indica el número de edición: "))
+            if anio>=0 and edicion>0:
+                revista=Revista(titulo, autor, anio, edicion)
+            else:
+                print("Los datos introducidos no son correctos, el año y la edición no pueden ser negativos.")
+        case 3:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            if libro:
+                print(biblioteca.agregar_material(libro))
+            if revista:
+                print(biblioteca.agregar_material(revista))
+            else:
+                print("Debes crear una revista o libro antes de agregarlo a la biblioteca.")
+        case 4:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            print(biblioteca.mostrar_materiales())
+        case 5:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            t=input("Que título buscas: ").lower()
+            print(biblioteca.buscar_por_titulo(t))
+        case 6:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            if libro:
+                print(libro.prestar())
+            if revista:
+                print(revista.prestar())
+            else:
+                print("Debes crear una revista o un libro primero.")
+        case 7:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            if libro:
+                print(libro.devolver())
+            if revista:
+                print(revista.devolver())
+            else:
+                print("Debes crear una revista o un libro primero.")
+        case 8:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            if libro:
+                t=input("Indica el nuevo título: ").lower()
+                print(libro.set_titulo(t))
+            if revista:
+                t=input("Indica el nuevo título: ").lower()
+                print(revista.set_titulo(t))
+            else:
+                print("Debes crear una revista o un libro primero.")
+        case 9:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            if libro:
+                a=input("Indica el nuevo autor: ").lower()
+                print(libro.set_autor(a))
+            if revista:
+                a=input("Indica el nuevo autor: ").lower()
+                print(revista.set_autor(a))
+            else:
+                print("Debes crear una revista o un libro primero.")
+        case 10:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            if libro:
+                p=int(input("Indica el nuevo año de publicación: "))
+                print(libro.set_anio_publicacion(p))
+            if revista:
+                p=int(input("Indica el nuevo año de publicación: "))
+                print(revista.set_anio_publicacion(p))
+            else:
+                print("Debes crear una revista o un libro primero.")
+        case 11:
+            print(f"Has escogido {lista_menu[opcion-1]}")
+            salir=True
+            print("Has salido del programa.")
+        case other:
+            print("Opción incorrecta. Pulsa del 1 al 11.")
