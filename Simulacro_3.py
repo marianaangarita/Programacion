a24b229cf7c3d9e293ebe9321d6b4cf6ce2d4a32
marianaangarita <mariana.angarita.afonso@gmail.com>
2026-02-11 15:58:01 +0100

    simulacro examen 3

---
+++ Simulacro_3.py
@@ -0,0 +1,55 @@
+class Curso():
+    def __init__(self,c,n,h,p):
+        self.codigo=c
+        self.nombre=n
+        self.hora=h
+        self.precio=p
+    
+    def mostrar_info(self):
+        return(f"Código: {self.codigo} | Nombre: {self.nombre} | Hora: {self.hora}h | Precio {self.precio}€")
+    
+    def calcular_precio(self):
+        return(f"{self.precio} €")
+
+class CursoOnline(Curso):
+    def __init__(self, c, n, h, p, plataforma):
+        super().__init__(c, n, h, p)
+        self.plataforma=plataforma
+    
+    def mostrar_info(self):
+        return (f"{super().mostrar_info()}| Plataforma: {self.plataforma}")
+
+class CursoPresencial(Curso):
+    def __init__(self, c, n, h, p, aula):
+        super().__init__(c, n, h, p)
+        self.aula=aula
+    def calcular_precio(self):
+        if self.hora>40:
+            descuento=self.precio*0.15
+            self.precio=self.precio+descuento
+            return (f"{self.precio} €")
+        else:
+            return super().calcular_precio()
+
+cursos={}
+
+def agregar_curso(codigo,objeto):
+    cursos[codigo]=objeto
+
+def mostrar_cursos():
+    for clave, valor in cursos.items():
+        print(f"Código {clave}: {valor.mostrar_info()}")
+
+def calcular_precio_final(codigo):
+    for clave, valor in cursos.items():
+        if clave==codigo:
+            return valor.calcular_precio()
+        
+lista_menu=["AÑADIR CURSO", "MOSTRAR CURSOS", "CALCULAR PRECIO FINAL", ""]
+def menu():
+    print("*************************")
+    print("MENÚ PRINCIPAL")
+    
+
+
+
