1cd5e5161e5d77f334f1ffb3eab92a57eeca85df
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-03-15 13:27:46 +0100

    commienzo del ejercicio

--- Sistema_Gestion_Documentos.py
+++ Sistema_Gestion_Documentos.py
@@ -23,5 +23,47 @@ with open("datos.json", "r") as archivo:
 def organizar_archivos():
     for archivo in os.listdir("."):
         if os.path.isfile(archivo) and archivo != "log.txt" and archivo != "datos.json":
+            for ext, carpeta in datos.items():
+                partes=archivo.split(".")
+                if partes[len(partes)-1]==ext:
+                    if not os.path.exists(carpeta):
+                        os.mkdir(carpeta)
+                    try:
+                        destino= os.path.join(carpeta, archivo)
+                        if os.path.exists(destino):
+                            print("El archivo ya existe en la carpeta destino")
+                            break
+                        else:
+                            shutil.move(archivo,carpeta)
+                            with open("log.txt", "a") as archivoLog:
+                                now = datetime.now()
+                                formatted = now.strftime("[%Y-%m-%d %H:%M:%S]")
+                                archivoLog.write(f"{formatted} Movido {archivo} a {carpeta}\n")
+                            break
+                    except FileExistsError:
+                        print("El nombre del archivo ya existe en la carpeta")
+                    except PermissionError:
+                        print("El archivo está en uso o falta privilegios del administrador.")
+
             
+def busqueda_avanzada():
+
+def copia_seguridad():
+
+def gestion_permisos():
+    usuario=input("Indica tu usuario: ").lower()
+    contrasena=input("Indica tu contraseña: ")
+    with open("base_deDatos.csv", "r") as archivo:
+        next(archivo)
+        for linea in archivo:
+            comprobacion=linea.split(",")
+            if comprobacion[0]==usuario and comprobacion[len(comprobacion)-1]==contrasena:
+                print("Acceso concedido, tienes permisos de administrador")
+                break
+            else:
+                print("Usuario o contraseña no válidos, vuelve a intentarlo")
+                continue
+            
+
+
 
