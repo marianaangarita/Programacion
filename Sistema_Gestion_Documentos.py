1b6c88a37b7b3367cc399079ef18be434890ed22
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-03-21 20:38:55 +0100

    mejoras con el bucle while

--- Sistema_Gestion_Documentos.py
+++ Sistema_Gestion_Documentos.py
@@ -78,15 +78,25 @@ def busqueda_avanzada(nombre_archivo):
 
 
 def copia_seguridad(archivo):
-    if archivo != "log.txt" and archivo != "datos.json":
-        if not os.path.exists("backup"):
+    existe=False
+    if not os.path.exists("backup"):
             os.mkdir("backup")
-        shutil.copy(f"{archivo}",f"backup/{archivo}")
-        print("Se ha hecho una copia de seguridad")
-        with open("log.txt", "a") as archivoLog:
-            now = datetime.now()
-            formatted = now.strftime("[%Y-%m-%d %H:%M:%S]")
-            archivoLog.write(f"{formatted} Se ha creado una copia de seguridad de {archivo} a backup\n")
+    for ruta_actual, subcarpetas, archivos in os.walk("."):
+        for listado in archivos:
+            if listado==archivo:
+                existe=True
+                copiaSeguridad=os.path.join(ruta_actual,listado)
+                shutil.copy(f"{copiaSeguridad}",f"backup/{archivo}")
+                print("Se ha hecho una copia de seguridad")
+                with open("log.txt", "a") as archivoLog:
+                    now = datetime.now()
+                    formatted = now.strftime("[%Y-%m-%d %H:%M:%S]")
+                    archivoLog.write(f"{formatted} Se ha creado una copia de seguridad de {archivo} a backup\n")
+                break
+        if existe==True:
+            break
+    if existe==False:
+        print("No hay un archivo con ese nombre")   
 
 def restaurar_copia_seguridad(nombre_archivo):
     copiaSeguridad= os.path.join("backup", nombre_archivo)
@@ -126,41 +136,43 @@ def gestion_permisos():
         if identificado==False:
             print("Usuario o contraseña no válidos, vuelve a intentarlo")
             return False
-           
 
-if gestion_permisos():
-    while not salir:
-        menu()
-        opcion=int(input("Escoge una opción: "))
+identificado= False 
+      
 
-        match opcion:
-            case 1:
-                print(f"Has elegido: {opciones_menu[opcion-1]}")
-                organizar_archivos()
-            case 2:
-                print(f"Has elegido: {opciones_menu[opcion-1]}")
-                nombre_archivo=input("Indica el nombre del archivo: ")
-                busqueda_avanzada(nombre_archivo)
-            case 3:
-                print(f"Has elegido: {opciones_menu[opcion-1]}")
-                archivo=input("indica el archivo que deseas hacer copia de seguridad: ")
-                copia_seguridad(archivo)
-            case 4:
-                print(f"Has elegido: {opciones_menu[opcion-1]}")
-                nombreArchivo=input("Indica el nombre de archivo: ")
-                restaurar_copia_seguridad(nombreArchivo)
-            case 5:
-                print(f"Has elegido: {opciones_menu[opcion-1]}")
-                archivo_nombre=input("Indica un nombre de archivo: ")
-                eliminar_archivo(archivo_nombre)
-            case 6:
-                print(f"Has elegido: {opciones_menu[opcion-1]}")
-                salir=True
-                print("Has salido del programa")
-            case __:
-                print("Opción incorrecta")
-else:
-    print("Usuario o contraseña no válidos, vuelve a intentarlo")
+while not identificado: 
+    identificado=gestion_permisos()     
+while not salir:
+    menu()
+    opcion=int(input("Escoge una opción: "))
+
+    match opcion:
+        case 1:
+            print(f"Has elegido: {opciones_menu[opcion-1]}")
+            organizar_archivos()
+        case 2:
+            print(f"Has elegido: {opciones_menu[opcion-1]}")
+            nombre_archivo=input("Indica el nombre del archivo: ")
+            busqueda_avanzada(nombre_archivo)
+        case 3:
+            print(f"Has elegido: {opciones_menu[opcion-1]}")
+            archivo=input("indica el archivo que deseas hacer copia de seguridad: ")
+            copia_seguridad(archivo)
+        case 4:
+            print(f"Has elegido: {opciones_menu[opcion-1]}")
+            nombreArchivo=input("Indica el nombre de archivo: ")
+            restaurar_copia_seguridad(nombreArchivo)
+        case 5:
+            print(f"Has elegido: {opciones_menu[opcion-1]}")
+            archivo_nombre=input("Indica un nombre de archivo: ")
+            eliminar_archivo(archivo_nombre)
+        case 6:
+            print(f"Has elegido: {opciones_menu[opcion-1]}")
+            salir=True
+            print("Has salido del programa")
+        case __:
+            print("Opción incorrecta")
+
 
         
         
