fec40d4feb90e85dc35a0603b6945e285d7af243
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-03-25 21:02:43 +0100

    mejoras

--- Simulacro_V2.py
+++ Simulacro_V2.py
@@ -8,3 +8,20 @@ def leer_texto(ruta_fichero):
         return ListaArchivo
 
 print(leer_texto("./archivo.txt"))
+
+def calcular_frecuencias(lista_palabras):
+    dic={}
+    for palabra in lista_palabras:
+        if palabra in dic:
+            dic[palabra]+=1
+        else:
+            dic[palabra]=1
+    return dic
+
+print(calcular_frecuencias(leer_texto("./archivo.txt")))
+
+def generar_ranking(diccionario_frecuencias):
+    tuplas_ordenadas = sorted(diccionario_frecuencias.items(), key=lambda x: x[1], reverse=True)
+    return(tuplas_ordenadas)
+
+print(generar_ranking(calcular_frecuencias(leer_texto("./archivo.txt"))))
