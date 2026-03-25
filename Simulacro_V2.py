2e80c394cd1bdf1f8a47fb7337491fc844a93a55
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-03-25 20:46:39 +0100

    creacion de archivo.txt

--- Simulacro_V2.py
+++ Simulacro_V2.py
@@ -1,3 +1,10 @@
 def leer_texto(ruta_fichero):
     with open(ruta_fichero, "r") as archivo:
-        
+        LecturaArchivo= archivo.read()
+        ArchivoSinComas = LecturaArchivo.replace(",","")
+        ArchivoSinPuntos = ArchivoSinComas.replace(".","")
+        ArchivoEnMinusculas = ArchivoSinPuntos.lower()
+        ListaArchivo = ArchivoEnMinusculas.split(" ")
+        return ListaArchivo
+
+print(leer_texto("./archivo.txt"))
