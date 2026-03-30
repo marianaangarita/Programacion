4646a598fd8bf303b6548f2074129527dafc7c26
marianaangarita <mariana.angarita.afonso@gmail.com>
2026-03-30 19:10:40 +0200

    mejoras

--- ejercicioAPI.py
+++ ejercicioAPI.py
@@ -12,11 +12,14 @@ def sacar_carta():
                 valor_carta=13
             case "JACK":
                 valor_carta=11
+            case "ACE":
+                valor_carta=1
+            case __:
+                valor_carta=int((data["cards"][0]["value"]))
         return valor_carta
     else:
         return("Error en la petición", response.status_code)
     
-print(sacar_carta())
 
 acierto=0
 salir=False
@@ -26,19 +29,22 @@ while not salir:
     segunda_carta=sacar_carta()
 
     print(f"La primera carta es {primera_carta}")
-    apuesta=input("¿Crees que la siguiente carta será mayor o menor que la segunda?: ").lower()
-
-    if primera_carta>segunda_carta and apuesta=="mayor":
-        acierto+=1
-        print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! ")
+    apuesta=input("¿Crees que la siguiente carta será mayor o menor que la primera?: ").lower()
+    try:
+        if segunda_carta>primera_carta and apuesta=="mayor":
+            acierto+=1
+            print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! ")
+            continue
+        if segunda_carta<primera_carta and apuesta=="menor":
+            acierto+=1
+            print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! ")
+            continue
+        else:
+            salir=True
+            print(f"Fallaste... has acertado {acierto} veces")
+    except ValueError:
+        print("Opcion no válida vuelve a intentarlo")
         continue
-    if primera_carta<segunda_carta and apuesta=="menor":
-        acierto+=1
-        print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! ")
-        continue
-    else:
-        salir=True
-        print(f"Fallaste... has acertado {acierto} veces")
 
 
              
