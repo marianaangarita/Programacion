ea651bb9c876234adf58f566d0b20c681fefbf08
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-04-08 19:58:12 +0200

    mejoras del programa, mejora del metodo sacar_carta()

--- ejercicioAPI.py
+++ ejercicioAPI.py
@@ -1,9 +1,15 @@
+'''
+1. Crea el juego superior/inferior haciendo peticiones a la api https://www.deckofcardsapi.com/
+2. Crea un sistema de vidas que cuando aciertes, continues y cuando falles la siguiente carta te reste una vida.
+'''
+
 import requests
 
-def sacar_carta():
-    response = requests.get("https://www.deckofcardsapi.com/api/deck/new/draw/?count=2")
+def sacar_carta(id_mazo):
+    response = requests.get(f"https://www.deckofcardsapi.com/api/deck/{id_mazo}/draw/?count=1")
     if response.status_code == 200:
         data = response.json()  
+        #cuantas_cartas_quedan = data["remaining"]
         valor_carta=(data["cards"][0]["value"])
         match valor_carta:
             case "QUEEN":
@@ -17,31 +23,54 @@ def sacar_carta():
             case __:
                 valor_carta=int((data["cards"][0]["value"]))
         return valor_carta
+    
+    #if cuantas_cartas_quedan == 0:
+
     else:
         return("Error en la petición", response.status_code)
     
 
-acierto=0
-salir=False
+acierto = 0
+vidas = 10
 
-while not salir:
-    primera_carta=sacar_carta()
-    segunda_carta=sacar_carta()
+respuesta = requests.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 
-    print(f"La primera carta es {primera_carta}")
+datos = respuesta.json()
+
+
+
+id_mazo_final = datos["deck_id"]
+
+carta_actual=sacar_carta(id_mazo_final)
+
+while vidas > 0:
+
+    print(f"\n Tienes {vidas} y llevas {acierto} aciertos.")
+
+    print(f"La carta actual es: {carta_actual}")
+
     apuesta=input("¿Crees que la siguiente carta será mayor o menor que la primera?: ").lower()
+
+    segunda_carta=sacar_carta(id_mazo_final)
+
     try:
-        if segunda_carta>primera_carta and apuesta=="mayor":
+        if segunda_carta>carta_actual and apuesta=="mayor":
             acierto+=1
-            print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! ")
-            continue
-        if segunda_carta<primera_carta and apuesta=="menor":
+            print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! Número de vidas: {vidas}")    
+
+        elif segunda_carta<carta_actual and apuesta=="menor":
             acierto+=1
-            print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! ")
-            continue
+            print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! Número de vidas: {vidas}")
+           
+        elif segunda_carta == carta_actual:
+            print("¡Ha sido empate! no te suma aciertos ni fallos, toca probar la siguiente ronda!")
+
         else:
-            salir=True
-            print(f"Fallaste... has acertado {acierto} veces")
+            vidas -= 1
+            print(f"Fallaste... la segunda carta era: {segunda_carta}. Has acertado {acierto} veces. Número de vidas: {vidas}")
+        
+        carta_actual=segunda_carta
+
     except ValueError:
         print("Opcion no válida vuelve a intentarlo")
         continue
