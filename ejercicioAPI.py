e13f6ffc5684f6dbcf887050f95b6822279f7973
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-04-10 20:18:28 +0200

    fin del ejercicio

--- ejercicioAPI.py
+++ ejercicioAPI.py
@@ -5,87 +5,95 @@
 
 import requests
 
-import sys  # para parar el programa
-
 acierto = 0
 
 vidas = 10
 
-respuesta = requests.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-
-datos = respuesta.json()
-
-id_mazo_final = datos["deck_id"]
-
 def sacar_carta(id_mazo):
-    response = requests.get(f"https://www.deckofcardsapi.com/api/deck/{id_mazo}/draw/?count=1")
-    if response.status_code == 200:
-        data = response.json()  
-        cuantas_cartas_quedan = data["remaining"]
-        if cuantas_cartas_quedan > 0:
-            valor_carta=(data["cards"][0]["value"])
-            match valor_carta:
-                case "QUEEN":
-                    valor_carta=12
-                case "KING":
-                    valor_carta=13
-                case "JACK":
-                    valor_carta=11
-                case "ACE":
-                    valor_carta=1
-                case __:
-                    valor_carta=int((data["cards"][0]["value"]))
-            return valor_carta
+    try:
+        response = requests.get(f"https://www.deckofcardsapi.com/api/deck/{id_mazo}/draw/?count=1")
+        if response.status_code == 200:
+            data = response.json()  
+            
+            if  len(data["cards"]) > 0:
+
+                valor_carta=(data["cards"][0]["value"])
+
+                match valor_carta:
+                    case "QUEEN":
+                        valor_carta=12
+                    case "KING":
+                        valor_carta=13
+                    case "JACK":
+                        valor_carta=11
+                    case "ACE":
+                        valor_carta=1
+                    case __:
+                        valor_carta = int(valor_carta)
+                return valor_carta
     
-        else:
-            return None
+            else:
+                return None
 
-    else:
-        return("Error en la petición", response.status_code)
+    except requests.exceptions.RequestException as e:
+       
+        print(f"Hubo un problema de conexión: {e}")
+
+        return None
+
+
+
+try:
+    respuesta = requests.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+
+    datos = respuesta.json()
+
+    id_mazo_final = datos["deck_id"]
     
-carta_actual=sacar_carta(id_mazo_final)
-
-if carta_actual is None:
-    print("¡Se han acabado las cartas del mazo! Juego terminado")
-    sys.exit() # se para el programa
+    carta_actual=sacar_carta(id_mazo_final)
     
+    if carta_actual:    
+        while vidas > 0:
+
+            print(f"\n Tienes {vidas} vidas y llevas {acierto} aciertos.")
+
+            print(f"La carta actual es: {carta_actual}")
+
+            apuesta=input("¿Crees que la siguiente carta será mayor o menor que la primera?: ").lower()
+
+            if apuesta not in ["mayor", "menor"]:
+                print(" Por favor, escribe 'mayor' o 'menor'.")
+                continue
+
+            segunda_carta=sacar_carta(id_mazo_final)
+
+            if segunda_carta is None:
+                print("¡Se han acabado las cartas del mazo! Juego terminado")
+                break
     
-while vidas > 0:
+            if segunda_carta>carta_actual and apuesta=="mayor":
+                acierto+=1
+                print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! Número de vidas: {vidas}")    
 
-    print(f"\n Tienes {vidas} vidas y llevas {acierto} aciertos.")
-
-    print(f"La carta actual es: {carta_actual}")
-
-    apuesta=input("¿Crees que la siguiente carta será mayor o menor que la primera?: ").lower()
-
-    if apuesta not in ["mayor", "menor"]:
-        print(" Por favor, escribe 'mayor' o 'menor'.")
-        continue
-
-    segunda_carta=sacar_carta(id_mazo_final)
-
-    if segunda_carta is None:
-        print("¡Se han acabado las cartas del mazo! Juego terminado")
-        break
-    
-    if segunda_carta>carta_actual and apuesta=="mayor":
-        acierto+=1
-        print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! Número de vidas: {vidas}")    
-
-    elif segunda_carta<carta_actual and apuesta=="menor":
-        acierto+=1
-        print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! Número de vidas: {vidas}")
+            elif segunda_carta<carta_actual and apuesta=="menor":
+                acierto+=1
+                print(f"Has acertado, la segunda carta era {segunda_carta}, sigues jugando! Número de vidas: {vidas}")
            
-    elif segunda_carta == carta_actual:
-        print("¡Ha sido empate! no te suma aciertos ni fallos, toca probar la siguiente ronda!")
+            elif segunda_carta == carta_actual:
+                print("¡Ha sido empate! no te suma aciertos ni fallos, toca probar la siguiente ronda!")
 
-    else:
-        vidas -= 1
-        print(f"Fallaste... la segunda carta era: {segunda_carta}. Has acertado {acierto} veces. Número de vidas: {vidas}")
+            else:
+                vidas -= 1
+                print(f"Fallaste... la segunda carta era: {segunda_carta}. Has acertado {acierto} veces. Número de vidas: {vidas}")
         
-    carta_actual=segunda_carta
+            carta_actual=segunda_carta
 
-
-print("--- FIN DEL JUEGO ---")
-print(f"Puntuación final: {acierto} aciertos.")
+        print("--- FIN DEL JUEGO ---")
+        print(f"Puntuación final: {acierto} aciertos.")
              
+    else:
+        print("No se pudo iniciar el juego. Revisa tu conexión o el mazo.")
+
+except requests.exceptions.RequestException as e:
+    print(f"Hubo un problema de conexión: {e}")
+    
